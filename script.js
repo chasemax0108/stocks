d9520de2d40af54d9852ab6c163d24e8b779008a
cmaxfield0108 <chasemax@byu.edu>
2021-02-23 21:06:00 -0700

    Design finished, but no functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+//api key: pk_397a7a28162847798b82df02facf503c 
+//api documentation: https://iexcloud.io/docs/api/
+
+/*
+
+Ideas for the web page:
+A search bar to look up the data on any stock
+A stock information block
+A list of news articles on stocks
+Display the stock logos
+
+When you hover over the search results / news articles, they light up. When you click on a search result, the left panel updates.
+When you click on the news article, you are taken to the website with that article.
+*/
